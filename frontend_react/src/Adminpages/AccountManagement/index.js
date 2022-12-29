6bc1420e557c530b386dfa9ce6fa5534ddf64a59
TrnThSn <saophaisoan23112002@gmail.com>
2022-12-29 18:11:25 +0700

    update feature

--- frontend_react/src/Adminpages/AccountManagement/index.js
+++ frontend_react/src/Adminpages/AccountManagement/index.js
@@ -1,6 +1,62 @@
+import classNames from 'classnames/bind';
+import styles from './AccountManagement.module.scss';
+import React from 'react';
+import { useEffect, useState, useMemo } from 'react';
+
+import { Link } from 'react-router-dom';
+const cx = classNames.bind(styles);
+
 
 function AccountManagement() {
-    return <h1>Admin AccountManagement</h1>
+    const [accountType, setAccountType] = useState('store');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleSubmit = () => {
+        if ((username != '') && (password != '')) {
+            alert(`Đã tạo tài khoản thành công cho ${accountType}`);
+            console.log(accountType);
+            console.log(username);
+            console.log(password);
+        }
+    };
+
+    return (
+        <div className={cx('wrapper')}>
+            <button className={cx('return')}>
+                <Link to="/admin">
+                    <span className={cx('text_container')}>Return</span>
+                </Link>
+            </button>
+
+            <div className={cx('container')}>
+                <div className={cx('account')}>
+                    <input
+                        type="text"
+                        className={cx('username')}
+                        placeholder="Tên tài khoản"
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+
+                    <input
+                        type="password"
+                        className={cx('password')}
+                        placeholder="Mật khẩu"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <select className={cx('account_type')} onChange={(e) => setAccountType(e.target.value)}>
+                        <option value="store">Cửa hàng</option>
+                        <option value="manufacture">Nhà sản xuất</option>
+                        <option value="warranty_center">Trung tâm bảo hành</option>
+                    </select>
+                </div>
+                <button className={cx('submit')} onClick={handleSubmit}>
+                    <span className={cx('text_container')}>Tạo tài khoản</span>
+                </button>
+            </div>
+        </div>
+    );
 }
 
-export default AccountManagement
+export default AccountManagement;
--- frontend_react/src/Adminpages/Admin/index.js
+++ frontend_react/src/Adminpages/Admin/index.js
@@ -6,18 +6,19 @@ const cx = classNames.bind(styles);
 
 function Admin() {
     return (
-        <div className={cx('container')}>
-            
-            <div className={cx('function')}>
-                <Link className={cx('button')} to="/admin/productcategoryhome">
-                    Quản lý danh mục sản phẩm
-                </Link>
-                <Link className={cx('button')} to="/admin/accountmanagement">
-                    Cấp tài khoản
-                </Link>
-                <Link className={cx('button')} to="/admin/statics">
-                    Thống kê
-                </Link>
+        <div className={cx('wrapper')}> 
+            <div className={cx('container')}>
+                <div className={cx('function')}>
+                    <Link className={cx('button')} to="/admin/productcategoryhome">
+                        Quản lý danh mục sản phẩm
+                    </Link>
+                    <Link className={cx('button')} to="/admin/accountmanagement">
+                        Cấp tài khoản
+                    </Link>
+                    <Link className={cx('button')} to="/admin/statics">
+                        Thống kê
+                    </Link>
+                </div>
             </div>
         </div>
     );
